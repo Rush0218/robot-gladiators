4e2eaedb635c1807063517894d60cc32ddc3e263
solera2 <rushtre1@gmail.com>
2021-10-14 23:37:01 -0400

    added conditions(if/else) to JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,45 @@
 var playerName = window.prompt("What is your robot's name?")
-// Note he lack of quotation marks around playerName
-console.log(playerName); 
-console.log("This logs a string, good for leaving yourself a message"); 
-console.log(10 +10); 
-console.log("Our robot's name is " + playerName); 
+var playerHealth = 100; 
+var playerAttack = 10; 
 
+// you can also log multiple values at one like this
+console.log(playerName, playerHealth, playerAttack); 
 
-// this creates a function name "fight"
+var enemyName = "Roberto"; 
+var enemyHealth = 50; 
+var enemyAttack = 12; 
 
-function fight() {
-    window.alert("The fight has begun!");
+var fight = function  () {
+    window.alert("Welcome to Robot Gladiators!")
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack; 
+    // Log a resulting message to the console so we know that it worked.
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "); 
+
+// Check enemy's health
+if(enemyHealth <= 0) {
+    window.alert(enemyName + " has died!"); 
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.")
 }
 
-// fight(); 
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack; 
+    // Log a resulting message to the console so we know that it worked.
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "); 
+
+// Check our robot's health
+if(playerName <= 0) {
+    window.alert(playerName + " has died!"); 
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.")
+}
+
+
+}; 
+
+
+fight(); 
